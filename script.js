66c4d4ab9a5ad413d92fb97ff12213c0bac105de
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-08-25 17:07:49 -0400

    still adding code from another repository that I made while in class in class i figured out how to make the boxes and add a save button that will commit things to local storage

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 var dt = new Date();
 document.getElementById('date-time').innerHTML=dt;
+
+$('.container').on('click', '.grade', function()  {
+    var parentId = $(this).parent().attr('data-id');
+     var name = ($(this).text());
+     console.log(name);
+     localStorage.setItem(parentId, name);
+ });
+ $('.name').each(function() {
+    var parentId = $(this).parent().attr('data-id');
+    var name = localStorage.getItem(parentId);
+    $(this).val(name);
+ });
