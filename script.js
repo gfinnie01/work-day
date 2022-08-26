82fc1c9514ef7d0d1d0c51b0ce7ce48a8c841c87
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-08-25 20:33:01 -0400

    not as hard as I thought it would be I am going to tuch up my css but other that that it should all be done

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var dt = new Date();
-document.getElementById('date-time').innerHTML=dt;
 
-$('.container').on('click', '.grade', function()  {
+
+$('.container').on('click', '.save', function()  {
     var parentId = $(this).parent().attr('data-id');
-     var name = ($(this).text());
-     console.log(name);
-     localStorage.setItem(parentId, name);
+    var name = $(this).prev().val();
+    console.log(name);
+    localStorage.setItem(parentId, name);
  });
- $('.name').each(function() {
+ $( '.name' ).each(function() {
     var parentId = $(this).parent().attr('data-id');
-    var name = localStorage.getItem(parentId);
-    $(this).val(name);
- });
+   var name = localStorage.getItem(parentId);
+   $(this).val(name);
+  });
+
+
+  var dt = new Date();
+  document.getElementById('date-time').innerHTML=dt;
